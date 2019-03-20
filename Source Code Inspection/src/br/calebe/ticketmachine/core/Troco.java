a2341f66b0b2bfae3cec805879b8300e2b8ef5e1
gianpgp <gianpgp@primeinformatica.com.br>
2019-03-20 12:32:04 -0300

    Fix E3

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -13,41 +13,41 @@ class Troco {
     public Troco(int valor) {
         papeisMoeda = new PapelMoeda[6];
         int count = 0;
-        while (valor % 100 != 0) {
+        while (valor % 100 != 0 && valor>100) {
             count++;
             valor = valor - 100;
         }
         papeisMoeda[5] = new PapelMoeda(100, count);
         count = 0;
-        while (valor % 50 != 0) {
+        while (valor % 50 != 0 && valor>50) {
             count++;
             valor = valor - 50;
 
         }
         papeisMoeda[4] = new PapelMoeda(50, count);
         count = 0;
-        while (valor % 20 != 0) {
+        while (valor % 20 != 0 && valor>20) {
             count++;
             valor = valor - 20;
 
         }
         papeisMoeda[3] = new PapelMoeda(20, count);
         count = 0;
-        while (valor % 10 != 0) {
+        while (valor % 10 != 0 && valor>10) {
             count++;
             valor = valor - 10;
 
         }
         papeisMoeda[2] = new PapelMoeda(10, count);
         count = 0;
-        while (valor % 5 != 0) {
+        while (valor % 5 != 0 && valor>5) {
             count++;
             valor = valor - 5;
 
         }
         papeisMoeda[1] = new PapelMoeda(5, count);
         count = 0;
-        while (valor % 2 != 0) {
+        while (valor % 2 != 0 && valor>2) {
             count++;
             valor = valor - 2;
 
