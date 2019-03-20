6cc7398c5abb40d57a4f64522712e8c36141f93d
gianpgp <gianpgp@primeinformatica.com.br>
2019-03-20 12:37:44 -0300

    Fix E9

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -79,7 +79,7 @@ class Troco {
 
         @Override
         public PapelMoeda next() {
-            PapelMoeda ret = null;
+            PapelMoeda ret = 0;
             for (int i = 6; i >= 0 && ret != null; i--) {
                 if (troco.papeisMoeda[i] != null) {
                     ret = troco.papeisMoeda[i];
