ad404433487cb39dc3088e41deca3651eebc9009
gianpgp <gianpgp@primeinformatica.com.br>
2019-03-20 12:36:08 -0300

    Fix E4

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -70,7 +70,7 @@ class Troco {
         @Override
         public boolean hasNext() {
             for (int i = 6; i >= 0; i--) {
-                if (troco.papeisMoeda[i] != null) {
+                if (troco.papeisMoeda[i] != 0) {
                     return true;
                 }
             }
