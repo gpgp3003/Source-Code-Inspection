e1910959be224567a8d4e0184d5481884bb04ac0
GP <gianpgazzillo99@gmail.com>
2019-03-29 23:06:35 -0300

    Fix - Tirando classe iterator da classe troco

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -59,41 +59,4 @@ class Troco {
         return new TrocoIterator(this);
     }
 
-    class TrocoIterator implements Iterator<PapelMoeda> {
-
-        protected Troco troco;
-
-        public TrocoIterator(Troco troco) {
-            this.troco = troco;
-        }
-
-        @Override
-        public boolean hasNext() {
-            for (int i = 6; i >= 0; i--) {
-                if (troco.papeisMoeda[i] != 0) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        @Override
-        public PapelMoeda next() {
-            PapelMoeda ret = 0;
-            while(hasNext()){
-                for (int i = 6; i >= 0 && ret != null; i--) {
-                    if (troco.papeisMoeda[i] != null) {
-                        ret += troco.papeisMoeda[i];
-                        troco.papeisMoeda[i] = null;
-                    }
-                }
-            }
-            return ret;
-        }
-
-        @Override
-        public void remove() {
-            next();
-        }
-    }
 }
