bd9aadcf54499d6913dbae7d34b72660043dcbcb
GP <gianpgazzillo99@gmail.com>
2019-03-30 01:12:26 -0300

    add more test TrocoTest

--- Source Code Inspection/src/br/calebe/ticketmachine/testes/TrocoTest.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/testes/TrocoTest.java
@@ -9,10 +9,20 @@ import org.junit.Test;
  * @author gpgp3003
  */
 public class TrocoTest {
+
    @Test
     public void testGetIterator() {
         Troco troco = new Troco(10);
         TrocoIterator ti = new TrocoIterator(troco);
         Assert.assertEquals(ti, troco.getIterator());
     }
+
+    @Test
+    public void testGetIteratorDois() {
+        Troco troco = new Troco(20);
+        TrocoIterator ti = new TrocoIterator(troco);
+        Assert.assertEquals(ti, troco.getIterator());
+    }
+
 }
+
