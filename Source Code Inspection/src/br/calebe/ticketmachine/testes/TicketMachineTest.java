bcda03a2f932705ed34d84d95a6825339d11c48c
Eduardo Tedeschi <eduardotedeschi97@gmail.com>
2019-03-29 23:32:34 -0300

    teste Inserir

---
+++ Source Code Inspection/src/br/calebe/ticketmachine/testes/TicketMachineTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.calebe.ticketmachine.core;
+
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author EduTedeschi
+ */
+public class TicketMachineTest {
+    
+    public void testInserir() throws PapelMoedaInvalidaException {
+        TicketMachine ticket = new TicketMachine(5);
+        int aux = ticket.getSaldo();
+        ticket.inserir(10);
+        Assert.assertTrue(aux < ticket.getSaldo());
+    }
+}
